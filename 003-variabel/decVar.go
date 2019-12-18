4ba044a4ba11f193a69c0b93f0a5989fa045e580
khanilaunggu <kanilaunggu@Kanis-MacBook-Pro.local>
2019-12-18 20:41:18 +0700

    semua file

---
+++ 003-variabel/decVar.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var s string
+
+	i = 50
+	s = "Sulawesi Selatan"
+
+	fmt.Println(i)
+	fmt.Println(s)
+}
---
+++ 003-variabel/decVarInit.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//inisiasi isi variabel
+	var i int = 10
+	var s string = "Sulawesi Selatan"
+
+	fmt.Println(i)
+	fmt.Println(s)
+}
