4a41cc569e423840a2b4f3cdd0b54b5381b64f39
Khani Launggu <kanilaunggu@gmail.com>
2019-12-18 15:55:47 +0700

    GO: pertama nilai.go

---
+++ 002-nilai/nilai.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Nilai String
+	fmt.Println("go" + "lang")
+
+	//Nilai Integer dan float
+	fmt.Println("1+4 =", 1+4)
+	fmt.Println("5.0/3.0 =", 5.0/3.0)
+
+	//nilai logika
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
